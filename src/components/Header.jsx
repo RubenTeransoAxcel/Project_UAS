af830b91fcfafcaeb28847c35d0afe159c27298c
RubenTeransoAxcel <ruben23ti@mahasiswa.pcr.ac.id>
2025-06-24 10:13:26 +0700

    logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -82,7 +82,7 @@ export default function Header() {
                 onClick={() => {
                   localStorage.removeItem("user");
                   localStorage.removeItem("accessToken");
-                  navigate("/login");
+                  navigate("/");
                 }}
                 className="w-full text-left px-4 py-2 text-red-600 hover:bg-gray-100"
               >
