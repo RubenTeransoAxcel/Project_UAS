c3f4d29e3d5de76e3602462ef0bd48ffae12891c
Axcel <ruben23ti@mahasiswa.pcr.ac.id>
2025-10-16 22:38:24 +0700

    Update artikelAPI.js

--- src/services/artikelAPI.js
+++ src/services/artikelAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_URL = "https://txhxqrwzwgivxvvnwpnk.supabase.co/rest/v1/artikel"
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4aHhxcnd6d2dpdnh2dm53cG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyODQ5NDIsImV4cCI6MjA2NDg2MDk0Mn0.QgAyKR-P6khZsSWtG5Su1UOMPqS5piCqZ7WT5mAh4TY"
+const API_URL = "https://rajpvmfarysrjfyncnhe.supabase.co/rest/v1/artikel"
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhanB2bWZhcnlzcmpmeW5jbmhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MjM4MDUsImV4cCI6MjA3NjE5OTgwNX0.YHRF-mjpgvmb1k6kSfCaWZihZdQd4Cbw5pS1fULpVWE"
 
 const headers = {
     apikey: API_KEY,
